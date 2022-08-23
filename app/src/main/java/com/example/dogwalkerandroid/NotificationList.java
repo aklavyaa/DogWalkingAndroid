61ed23cb8a88be3eba2e83fe69274a15c11fe032
Z3N00 <78555919+Gurwindersingh85@users.noreply.github.com>
2022-08-22 23:58:31 -0400

    Update NotificationList.java
    Notificationlist added

--- app/src/main/java/com/example/dogwalkerandroid/NotificationList.java
+++ app/src/main/java/com/example/dogwalkerandroid/NotificationList.java
@@ -1,13 +1,33 @@
 package com.example.dogwalkerandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.RelativeLayout;
 
-public class NotificationList extends AppCompatActivity {
+import com.example.dogwalkerandroid.utils.NotificationModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class NotificationList extends AppCompatActivity implements NotificationAdap.clickdelete {
+
+
+    private FirebaseFirestore db;
+    private RecyclerView recyclerView;
+    private NotificationAdap adap;
+    private String TAG = NotificationList.class.getSimpleName();
+    private String docId = "";
+    private ArrayList<NotificationModel> list = new ArrayList<>();
 
 
 
@@ -15,11 +35,61 @@ public class NotificationList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_list);
+        db = FirebaseFirestore.getInstance();
 
-        RecyclerView recyclerView = findViewById(R.id.recylcer_list);
+
+        findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                list.clear();
+                adap.notifyDataSetChanged();
+            }
+        });
+        adap = new NotificationAdap(this,list,this);
+         recyclerView = findViewById(R.id.recylcer_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new NotificationAdap(this));
+        recyclerView.setAdapter(adap);
+        db.collection("DogOwner").whereEqualTo("user_id",SharedPref.getString("user_id","")).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.e(TAG, document.getId() + " => " + document.getData());
+                        docId = document.getId();
+
+                    }
+
+                    db.collection("DogOwner").document(docId).collection("WalkerRequest").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+
+                            for (QueryDocumentSnapshot snap : task.getResult()){
+                               String availability = snap.getData().get("availablity").toString();
+                               String exp = snap.getData().get("experience").toString();
+                               String hrs =  snap.getData().get("hrs").toString();
+                               String price =  snap.getData().get("price").toString();
+                               String username =  snap.getData().get("user_name").toString();
+                               list.add(new NotificationModel(username,exp,hrs,price,availability));
+                            }
+
+
+                            adap.notifyDataSetChanged();
+
+                        }
+                    });
+
+
+                }
+            }
+        });
 
     }
 
+    @Override
+    public void deleteitem(int position) {
+        list.remove(position);
+        adap.notifyDataSetChanged();
+
+    }
 }
